eddcaf27f092e02f3accfff1145f69dc603fa193
awsomewebsite <bharatsh7417@gmail.com>
2023-04-08 07:18:54 -0700

    digitech react
    clone website

---
+++ digitech/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./component/header";
+import './App.css';
+import './component/script';
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    </>
+  );
+}
+
+export default App;
